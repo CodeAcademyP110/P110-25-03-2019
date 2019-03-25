76dccb6cee95e6e6218a3c998b3385ee418801c0
Samir Dadash-zada <samir.d@code.edu.az>
2019-03-25 13:06:32 +0400

    Add files via upload

---
+++ 25.03.2019/js/main.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// let age = prompt("Please, input your age: ");
+
+// console.log("Your age is " + age); //old-way
+
+// let ageNumber = +age;
+
+// if(!ageNumber)
+// {
+//     alert("Please, input valid age as number")
+// }
+// else
+// {
+//     alert(`Your age after 10 year will be ${ageNumber + 10}`); //es6
+// }
+
+let season = "Summer";
+
+if(season === "Spring")
+{
+    alert("Yazdir")
+}
+else if(season === "Winter")
+{
+    alert("Qisdir")
+}
+else if(season === "Summer")
+{
+    alert("Yaydir")
+}
+else
+{
+    alert("Payizdir")
+}
+
+
+
